a80f4cb2b795abe11d8406c846a5fa0e9b1d4e36
jamachado2 <jamachado2@miners.utep.edu>
2020-04-20 09:28:46 -0600

    triangle with 3 loops

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -5,6 +5,7 @@
 #include <libTimer.h>
 #include "lcdutils.h"
 #include "lcddraw.h"
+//#include "lcddrawx.h"
 
 /** Initializes everything, clears the screen, draws "hello" and a square */
 int
@@ -14,10 +15,35 @@ main()
   lcd_init();
   u_char width = screenWidth, height = screenHeight;
 
-  clearScreen(COLOR_BLUE);
+  clearScreen(COLOR_BLACK);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+    drawString5x7(25,20, "SCORE !!", COLOR_BLUE, COLOR_BLACK);
 
-  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+    //    fillRectangle(35, 35, 60, 60, COLOR_BLUE);
   
+  u_char j;
+
+  /* dot */
+  //for(j = 0; j < 90; j++)
+  //drawPixel(10, 10, COLOR_YELLOW);
+
+  /* right diagonal line*/
+  for(j = 0; j < 50; j++)
+  drawPixel(j, j, COLOR_BLUE);
+
+  /* vertical line */
+  for(j = 0; j < 50; j++)
+  drawPixel(0, j, COLOR_RED);
+
+  /* horizontal LINE */
+  for(j = 0; j < 50; j++)
+  drawPixel(j, 50, COLOR_YELLOW);
+
+  /* what's this a dot? */
+  //for(j = 0; j < 90; j++)
+  //drawPixel(j, -j, COLOR_WHITE);
+  
+  /* right diagonal line */
+  //for(j = 0; j > 90; j++)
+  //drawPixel(50, j, COLOR_RED);
 }
